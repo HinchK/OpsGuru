180d426a228433a0a76491dd4f1a1fbfa3cc68fa
manapause [hinchk] <kasey.hinchman@gmail.com>
2015-03-09 09:46:42 -0700

    apply styleci patch

--- app/Http/Controllers/DevCartController.php
+++ app/Http/Controllers/DevCartController.php
@@ -1,18 +1,16 @@
 <?php namespace OpsGuru\Http\Controllers;
 
-
 use Gloudemans\Shoppingcart\Facades\Cart;
 
-class DevCartController extends Controller{
-
+class DevCartController extends Controller
+{
     public function index()
     {
         Cart::add('192ao12', 'Product 1', 1, 9.99);
-        Cart::add('1239ad0', 'Product 2', 2, 5.95, array('size' => 'large'));
+        Cart::add('1239ad0', 'Product 2', 2, 5.95, ['size' => 'large']);
 
         $cart = Cart::content();
 
         return view('cart', compact('cart'));
     }
-
 }
